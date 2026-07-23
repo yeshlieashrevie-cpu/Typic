0d85cdc56add797e4cf8c822e13ef4840dabdce6
yeshlieashrevie-cpu <yeshlieashrevie@gmail.com>
2026-07-23 13:41:33 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
